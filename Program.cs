07c85b0abc353cd603743a17d0c31f31b533e265
Katya <katin27@yandex.ru>
2023-12-27 23:35:41 +0300

    new

--- Program.cs
+++ Program.cs
@@ -2,5 +2,18 @@
 
 // тип данных,имя переменной = значение;
 // camelCase: firstNumber, secoydNumber
-int number = 9;
-int result = number * number; 
+// int number = 9;
+// int result = number * number; 
+Console.WriteLine("введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber == secondNumber * secondNumber)
+{ 
+    Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
+}
+
+
+
