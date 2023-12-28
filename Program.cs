97fde6e503e37c406717e9f1cbb295da28b45858
Katya <katin27@yandex.ru>
2023-12-28 17:21:58 +0300

    new3

--- Program.cs
+++ Program.cs
@@ -18,17 +18,28 @@
 // Console.Write("введите число N: ");
 // int N = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите певрое число: "); 
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите певрое число: "); 
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber == secondNumber * secondNumber)
+// if(firstNumber == secondNumber * secondNumber)
+// {
+//    Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
+// }
+// else
+// {
+//    Console.WriteLine("нет, " + firstNumber + " не явл кв от числа " + secondNumber);
+// }
+
+Console.Write("Ввудите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// N =4,
+int negativeN = N * -1;  // -4 
+
+while (negativeN <= N)
 {
-   Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
-}
-else
-{
-   Console.WriteLine("нет, " + firstNumber + " не явл кв от числа " + secondNumber);
+    Console.Write(negativeN + " ");
+    negativeN++;
 }
