f543b19dad601613dc0f154f8aeb4f13d09c8df3
Katya <katin27@yandex.ru>
2023-12-28 16:42:54 +0300

    new2

--- Program.cs
+++ Program.cs
@@ -4,16 +4,31 @@
 // camelCase: firstNumber, secoydNumber
 // int number = 9;
 // int result = number * number; 
-Console.WriteLine("введите число: ");
+// Console.WriteLine("введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("введите число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber == secondNumber * secondNumber)
+// { 
+//     Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
+// }
+
+// Console.Write("введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите певрое число: "); 
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("введите число: ");
+Console.WriteLine("введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 if(firstNumber == secondNumber * secondNumber)
-{ 
-    Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
+{
+   Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
 }
-
-
-
+else
+{
+   Console.WriteLine("нет, " + firstNumber + " не явл кв от числа " + secondNumber);
+}
