798826bb1b77d5dde8175c6c551e43e88704e51e
Katya <katin27@yandex.ru>
2024-01-06 22:40:45 +0300

    DZ

--- Program.cs
+++ Program.cs
@@ -1,45 +1,45 @@
-﻿// комментарий
-
-// тип данных,имя переменной = значение;
-// camelCase: firstNumber, secoydNumber
-// int number = 9;
-// int result = number * number; 
-// Console.WriteLine("введите число: ");
-// int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("введите число: ");
-// int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-// if(firstNumber == secondNumber * secondNumber)
-// { 
-//     Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
-// }
-
-// Console.Write("введите число N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("введите певрое число: "); 
-// int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("введите второе число: ");
-// int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-// if(firstNumber == secondNumber * secondNumber)
+﻿// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// // "&&" - "И": яблоки и апельсины
+// // "||" - "ИЛИ" 
+// if (number >= 100 && number <= 999) // трехзначчное число
 // {
-//    Console.WriteLine("Да, " + firstNumber + " явл кв от числа " + secondNumber);
+//     int firstDigit = number / 100;
+//     int thitdDigit = number % 10;
+//     int result = firstDigit * 10 + thitdDigit;
+//     Console.WriteLine($"результат от числа {number} = {result}");
 // }
-// else
+// else // не трехзначное
 // {
-//    Console.WriteLine("нет, " + firstNumber + " не явл кв от числа " + secondNumber);
+//     Console.WriteLine("Число не трехзначное");
 // }
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ввудите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-// N =4,
-int negativeN = N * -1;  // -4 
+// if (number >= 100 && number <= 999)
+// {
+//     int sekondDigit = number / 10 % 10;
+//     int thirdDigit = number % 10;
+//     int result = (int)Math.Pow(sekondDigit, thirdDigit);
+//     Console.WriteLine($"Число {number}:{sekondDigit}^{thirdDigit} = {result}");
+// }
+// else // не трехзначное
+//  {
+//     Console.WriteLine("Число не трехзначное");
+//  }
 
-while (negativeN <= N)
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10 && number <= 99)
 {
-    Console.Write(negativeN + " ");
-    negativeN++;
+    int firstNumber = number / 10;
+    int secondNumber = number % 10;
+    int max = firstNumber;
+    while (secondNumber > max)
+    {
+        max = secondNumber;
+    }
+    Console.WriteLine(max);
 }
+
